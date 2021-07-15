e09c371c1245ed09b519429dbd009673a5f6552c
Abdulhafiz-Yuuf <talk2abdulhafiz@gmail.com>
2021-07-15 15:41:45 +0100

    transforming to NCS

--- client/src/views/Footer.js
+++ client/src/views/Footer.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='container bg-danger mt-5 rounded ' style={{ height: '80px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', fontSize: '1rem' }}>
+    <div className='container bg-success mt-5 rounded ' style={{ height: '80px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', fontSize: '1rem' }}>
       <div style={{ height: '20px' }}></div>
-      <p className='text-light'> www.9jablood.ng
+      <p className='text-light text-center'> A B.Sc Project by Rahmat Bello<br />
+        Department of Computer Science <br />
+        Ahmadu Bello University, Zaria
       </p>
     </div>
   )
--- client/src/views/NavBar.js
+++ client/src/views/NavBar.js
@@ -4,7 +4,7 @@ import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, Button } from 'react
 import LoginButton from '../components/Auth0LoginButton';
 import LogoutButton from '../components/Auth0LogoutButton';
 import { useAuth0 } from '@auth0/auth0-react';
-// import backgroundImage from './blood.jpeg'
+import logo from '../assets/logo.png'
 const NavBar = (props) => {
 
     const { isAuthenticated } = useAuth0()
@@ -22,39 +22,41 @@ const NavBar = (props) => {
 
                 color="success"
                 light expand="md">
-                <NavbarBrand to="/" className='font-weight-bold'> 9jaBlood</NavbarBrand>
+                <NavbarBrand to="/" className='font-weight-bold'>
+                    <img src={logo}
+                </NavbarBrand>
                 <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <ul className="nav">
-                            <li className="nav-item">
-                                <Button color='success'>
-                                    <Link className="nav-link active text-light font-weight-bold" to="/">Home</Link>
-                                </Button>
-                            </li>
-                            <li className="nav-item">
-                                <Button color='success '>
-                                    <Link className="nav-link active text-light font-weight-bold" to="/med-center">Medical Center</Link>
-                                </Button>
-                            </li>
-                            <li className="nav-item">
-                                <Button color='success'>
-                                    <Link className="nav-link active text-light font-weight-bold" to="/:userId/form">Find a Donor</Link>
-                                </Button>
-                            </li>
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <ul className="nav">
+                                <li className="nav-item">
+                                    <Button color='success'>
+                                        <Link className="nav-link active text-light font-weight-bold" to="/">Home</Link>
+                                    </Button>
+                                </li>
+                                <li className="nav-item">
+                                    <Button color='success '>
+                                        <Link className="nav-link active text-light font-weight-bold" to="/med-center">Medical Center</Link>
+                                    </Button>
+                                </li>
+                                <li className="nav-item">
+                                    <Button color='success'>
+                                        <Link className="nav-link active text-light font-weight-bold" to="/:userId/form">Find a Donor</Link>
+                                    </Button>
+                                </li>
 
-                            <li className="nav-item">
-                                <Button color='success '>
-                                    <Link className="nav-link active text-light font-weight-bold" to='/testPage'>Testing Page</Link>
-                                </Button>
-                            </li>
-                        </ul>
-                    </Nav>
-                    {!isAuthenticated ? <LoginButton /> :
-                        <LogoutButton />
-                    }
+                                <li className="nav-item">
+                                    <Button color='success '>
+                                        <Link className="nav-link active text-light font-weight-bold" to='/testPage'>Testing Page</Link>
+                                    </Button>
+                                </li>
+                            </ul>
+                        </Nav>
+                        {!isAuthenticated ? <LoginButton /> :
+                            <LogoutButton />
+                        }
 
-                </Collapse>
+                    </Collapse>
 
             </Navbar>
         </div >
